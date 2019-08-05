e49f48bc7ccc0b18ccb6e24df130e38600808582
Abdoul Diallo <diallo.abdoulrahime@gmail.com>
2019-08-05 20:31:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
 def consolidate_cart(cart)
-  # code here
+    keys = []
+  consolidate_cart = {}
+  cart.each do |x|
+    x.each do |k, v|
+      keys << k
+      consolidate_cart[k] = v
+      #binding.pry
+    end
+  end
+
+  cart.map do |x|
+    x.map do |k, v|
+      consolidate_cart[k][:count] = keys.count(k)
+    end
+  end
+  consolidate_cart
 end
 
 def apply_coupons(cart, coupons)
